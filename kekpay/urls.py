3d2c966581234e55849d2db1833c0ac9357cbfdb
Marat <57956285+mmkhitaryan@users.noreply.github.com>
2020-12-19 23:55:07 +0600

    token refresh view + JWT middleware by default

--- kekpay/urls.py
+++ kekpay/urls.py
@@ -3,11 +3,14 @@ from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from .users.views import ObtainChallengeJWT, AttemptChallenge
 
 router = DefaultRouter()
 
 urlpatterns = [
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/auth/obtain/', ObtainChallengeJWT.as_view(), name='obtain-token'),
     path('api/auth/attempt/', AttemptChallenge.as_view(), name='attempt-challenge')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
