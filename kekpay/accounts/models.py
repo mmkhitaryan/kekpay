4e1f400bd61e2d0a239ad64e61677748abd9de65
Marat <57956285+mmkhitaryan@users.noreply.github.com>
2021-01-03 21:55:52 +0600

    remove unused imports

--- kekpay/accounts/models.py
+++ kekpay/accounts/models.py
@@ -3,12 +3,10 @@ import uuid
 from django.db import models
 from django.db import transaction
 from django.utils import timezone
-from django.conf import settings
 
 from .exceptions import InsufficientFundsError
 from .currencies import currencies_choices, get_currency_by_code
 
-UserModel = settings.AUTH_USER_MODEL
 
 class TransactionHistory(models.Model):
     from_account = models.ForeignKey(
@@ -27,7 +25,6 @@ class TransactionHistory(models.Model):
         decimal_places=6,
     )
     created_at = models.DateTimeField(default=timezone.now)
-
 
 
 class Account(models.Model):
